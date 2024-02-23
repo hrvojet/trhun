912e149595410d0fa7a4cd21c7ae877e0182d820
Hrvoje <hrvojetrcak@gmail.com>
2024-02-23 22:15:19 +0100

    Add taiga-ui

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,32 @@
-import { Component } from '@angular/core';
+import {
+  TuiRootModule,
+  TuiDialogModule,
+  TuiThemeNightModule,
+  TuiModeModule,
+  TuiBrightness,
+  TuiNightThemeService,
+  TuiPrimitiveTextfieldModule
+} from "@taiga-ui/core";
+import {Component, Inject} from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { Observable } from "rxjs";
+import {TuiLetModule} from "@taiga-ui/cdk";
+import {AsyncPipe, NgIf} from "@angular/common";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, TuiRootModule, TuiDialogModule, TuiThemeNightModule, TuiModeModule, TuiLetModule, AsyncPipe, NgIf, TuiPrimitiveTextfieldModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
   title = 'trhun';
+
+
+  constructor(@Inject(TuiNightThemeService) readonly night$: Observable<boolean>) {}
+
+  /*get mode(): TuiBrightness | null {
+    return this.night. ? 'onDark' : null;
+  }*/
 }
--- src/app/cracker/cracker.component.ts
+++ src/app/cracker/cracker.component.ts
@@ -1,12 +1,28 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {TuiInputModule} from "@taiga-ui/kit";
+import {FormControl, FormGroup, FormsModule, ReactiveFormsModule} from "@angular/forms";
 
 @Component({
   selector: 'app-cracker',
   standalone: true,
-  imports: [],
+  imports: [
+    TuiInputModule,
+    FormsModule,
+    ReactiveFormsModule
+  ],
   templateUrl: './cracker.component.html',
   styleUrl: './cracker.component.scss'
 })
-export class CrackerComponent {
+export class CrackerComponent implements OnInit {
+
+  readonly inputForm = new FormGroup({
+    textValue: new FormControl('')
+  });
+
+
+  ngOnInit(): void {
+    this.inputForm.valueChanges.subscribe((value) => console.log(value));
+  }
+
 
 }
