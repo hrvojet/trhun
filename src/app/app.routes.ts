b67b6d71ba3eb0eb03b853a12132be6bad83eb00
Hrvoje <hrvojetrcak@gmail.com>
2024-02-23 19:02:04 +0100

    Add cracker component

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,9 @@
 import { Routes } from '@angular/router';
+import {CrackerComponent} from "./cracker/cracker.component";
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    component: CrackerComponent
+  }
+];
