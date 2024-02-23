551c2e49041f759cb15d1939abd18afb5b52b358
Hrvoje <hrvojetrcak@gmail.com>
2024-02-23 18:51:40 +0100

    Initial commit

---
+++ src/app/cracker/cracker.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-cracker',
+  standalone: true,
+  imports: [],
+  templateUrl: './cracker.component.html',
+  styleUrl: './cracker.component.scss'
+})
+export class CrackerComponent {
+
+}
